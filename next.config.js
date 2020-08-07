7c1e809a4bdb03f20ffa49f72bf463b69771de4e
Egidio Caprino <egidio.caprino@gmail.com>
2020-08-07 11:12:39 +0200

    fix(website): website does not load assets

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 const withCss = require('@zeit/next-css');
 
-const production = process.env.NODE_ENV === 'production';
-
-module.exports = withCss({
-  assetPrefix: production ? '/egidiocaprino.github.io/' : '',
-});
+module.exports = withCss({});
